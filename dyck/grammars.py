f4c499cb68244787a218471478e26bb9a5ebdb89
Orestis Melkonian <melkon.or@gmail.com>
2018-01-11 17:09:41 +0100

    Interactions with insertions (at the same time)

--- dyck/grammars.py
+++ dyck/grammars.py
@@ -71,11 +71,7 @@ def all_ordered(*orders):
 def all_o(lhs, rhs, *orders):
     return [(lhs, rhs, order) for order in all_ordered(*orders)]
 
-# def pp(s):
-#     print(s.replace('(0, 0)', 'x').replace('(0, 1)', 'y').replace('(1, 0)', 'z').replace('(1, 1)', 'w'))
-#
 # Grammar
-#
 g = lambda initial_symbol: Grammar(
 [
     # TOP
@@ -84,22 +80,12 @@ g = lambda initial_symbol: Grammar(
     # base
     [(v, [], [list(k[0]), list(k[1])]) for k, v in states.items() if v != 'W'],
 
+    # 2-ins
+    # [list(double_ins(v[0], v[1])) for v in states],
     # 3-ins
-    [list(triple_ins(v[0], v[1])) for v in states],
+    # [list(triple_ins(v[0], v[1])) for v in states],
 
     [list(prog(L, R)) for L, R in all_state_pairs()],
-    # [list(prog3(L, C, R)) for L, C, R in all_state_pairs3()],
-
-    # all_o('W', ['lABC'], [x, y]),
-    # all_o('W', ['lArBC'], [x, y]),
-    # all_o('W', ['lABrC'], [x, y]),
-    # all_o('W', ['rABC'], [x, y]),
-
-    all_o('W', ['W']*3, [x, y], [z, w], [l, m]),
-
-    # DEBUG
-    [('_' + k, [k], [[x, y]]) for k in all_states],
-    [('$_' + k, [k], [[x, '$', y]]) for k in all_states],
 
 ], topdown=True, filtered=True, initial_symbol=initial_symbol)
 
@@ -112,17 +98,6 @@ def all_state_pairs():
                 cur.append((L, R))
                 yield (L, R)
 
-def all_state_pairs3():
-    cur = []
-    for L in states:
-        for R in states:
-            for C in states:
-                # Exclude X-
-                if any(map(lambda t: '-' in states[t], [L, R, C])):
-                    continue
-                if all(map(lambda t: t not in cur, [(L, R, C), (R, L, C), (R, C, L), (C, L, R), (C, R, L)])):
-                    cur.append((L, C, R))
-                    yield (L, C, R)
 
 states = {
     # 0 Symbol
@@ -138,68 +113,16 @@ states = {
     ('', "c"): 'rC+',
     # 2 Symbols
     ("bc", ""): 'lA-',
-    ("cb", ""): 'ulA-',
     ("", "bc"): 'rA-',
-    ("", "cb"): 'urA-',
     ("b", "c"): 'lrA-',
-    ("c", "b"): 'ulrA-',
     # ------------------
     ("ac", ""): 'lB-',
-    ("ca", ""): 'ulB-',
     ("", "ac"): 'rB-',
-    ("", "ca"): 'urB-',
     ("a", "c"): 'lrB-',
-    ("c", "a"): 'ulrB-',
     # ------------------
     ("ab", ""): 'lC-',
-    ("ba", ""): 'ulC-',
     ("", "ab"): 'rC-',
-    ("", "ba"): 'urC-',
     ("a", "b"): 'lrC-',
-    ("b", "a"): 'ulrC-',
-    # ------------------
-    ("aa", ""): 'lA++',
-    ("a", "a"): 'lrA++',
-    ("", "aa"): 'rA++',
-    # ------------------
-    ("bb", ""): 'lB++',
-    ("b", "b"): 'lrB++',
-    ("", "bb"): 'rB++',
-    # ------------------
-    ("cc", ""): 'lC++',
-    ("c", "c"): 'lrC++',
-    ("", "cc"): 'rC++',
-    # ------------------
-    # 3 Symbols
-    # ("abc", ""): 'lABC',
-    # ("ab", "c"): 'lABrC',
-    # ("a", "bc"): 'lArBC',
-    # ("", "abc"): 'rABC',
-    # # ------------------
-    # ("acb", ""): 'lACB',
-    # ("ac", "b"): 'lACrB',
-    # ("a", "cb"): 'lArCB',
-    # ("", "acb"): 'rACB',
-    # # ------------------
-    # ("bac", ""): 'lBAC',
-    # ("ba", "c"): 'lBArC',
-    # ("b", "ac"): 'lBrAC',
-    # ("", "bac"): 'rBAC',
-    # # ------------------
-    # ("bca", ""): 'lBCA',
-    # ("bc", "a"): 'lBCrA',
-    # ("b", "ca"): 'lBrCA',
-    # ("", "bca"): 'rBCA',
-    # # ------------------
-    # ("cab", ""): 'lCAB',
-    # ("ca", "b"): 'lCArB',
-    # ("c", "ab"): 'lCrAB',
-    # ("", "cab"): 'rCAB',
-    # # ------------------
-    # ("cba", ""): 'lCBA',
-    # ("cb", "a"): 'lCBrA',
-    # ("c", "ba"): 'lCrBA',
-    # ("", "cba"): 'rCBA',
 }
 all_states = states.values()
 
@@ -227,14 +150,20 @@ def eliminate((l, r)):
             yield (l, r)
 
 
-def triple_ins(_x, _y):
+def double_ins(_x, _y):
     # _x: "b"
     # _y: "a"
     d = {x:_x, y:_y}
     rhs = states[(_x, _y)]
-    perms = all_ordered([x, y], [a], [b], [c])
+    perms = sum([
+        all_ordered([x, y], [k], [l])
+        for k in [a, b, c]
+        for l in [a, b, c]]
+        , [])
+
     def transform(l):
         return ''.join(map(lambda elem: d[elem] if isinstance(elem, tuple) else elem, l))
+
     perms2 = map(lambda (l1, l2): (transform(l1), transform(l2)), perms)
     for perm, _perm in zip(perms2, perms):
         eliminated = list(eliminate(perm))[0]
@@ -246,7 +175,32 @@ def triple_ins(_x, _y):
             continue
 
 
-def prog((_x, _y), (_z, _w)):
+def triple_ins(_x, _y):
+    # _x: "b"
+    # _y: "a"
+    d = {x:_x, y:_y}
+    rhs = states[(_x, _y)]
+    perms = all_ordered([x, y], [a], [b], [c])
+
+    def transform(l):
+        return ''.join(map(lambda elem: d[elem] if isinstance(elem, tuple) else elem, l))
+
+    perms2 = map(lambda (l1, l2): (transform(l1), transform(l2)), perms)
+    for perm, _perm in zip(perms2, perms):
+        eliminated = list(eliminate(perm))[0]
+        try:
+            state = states[eliminated]
+            rule = (state, [rhs], [_perm[0], _perm[1]])
+            yield rule
+        except KeyError:
+            continue
+
+
+def flatten(l):
+    return sum(l, [])
+
+
+def prog((_x, _y), (_z, _w), insertions=[], repetitions=0):
     L = states[(_x, _y)]
     R = states[(_z, _w)]
     """e.g.
@@ -255,20 +209,25 @@ def prog((_x, _y), (_z, _w)):
     _z: b
     _w: c
     """
-    d = {x:_x, y:_y, z:_z, w:_w}
-    for element1, element2 in all_ordered([x, y], [z, w]):
+    d = {x: _x, y: _y, z: _z, w: _w}
+    all_insertions = [e, [a], [b], [c], [a, b], [a, c], [b, c]]
+    all_combinations = flatten([
+        all_ordered([x, y], [z, w], *[[el] for el in to_insert])
+        for to_insert in all_insertions
+    ])
+    for element1, element2 in all_combinations:
         """e.g.
-        element1 = [x, z]
-        element2 = [y, w]
+        element1 = [x, z, a]
+        element2 = [y, b, w]
         """
         desc1, desc2 = "", ""
         for elem in element1:
-            desc1 += d[elem]
+            desc1 += d[elem] if isinstance(elem, tuple) else elem
         for elem in element2:
-            desc2 += d[elem]
+            desc2 += d[elem] if isinstance(elem, tuple) else elem
         descriptor = (desc1, desc2)
         """e.g.
-        descriptor = ("ab", "bc")
+        descriptor = ("abA", "bcB")
         """
         eliminated_list = eliminate(descriptor)
         """e.g.
@@ -289,51 +248,3 @@ def prog((_x, _y), (_z, _w)):
             eliminated_state = "rB+"
             """
             yield (eliminated_state, [L, R], [element1, element2])
-
-
-def prog3((_x, _y), (_z, _w), (_l, _m)):
-    L = states[(_x, _y)]
-    C = states[(_z, _w)]
-    R = states[(_l, _m)]
-    """e.g.
-    _x: a
-    _y: b
-    _z: b
-    _w: c
-    _l: e
-    _m: a
-    """
-    d = {x:_x, y:_y, z:_z, w:_w, l: _l, m: _m}
-    for element1, element2 in all_ordered([x, y], [z, w], [l, m]):
-        """e.g.
-        element1 = [x, z, l]
-        element2 = [y, w, m]
-        """
-        desc1, desc2 = "", ""
-        for elem in element1:
-            desc1 += d[elem]
-        for elem in element2:
-            desc2 += d[elem]
-        descriptor = (desc1, desc2)
-        """e.g.
-        descriptor = ("ab", "bc")
-        """
-        eliminated_list = eliminate(descriptor)
-        """e.g.
-        eliminatedList = [
-            (e, "b"),
-            ("b", e)
-        ]
-        """
-        for eliminated in eliminated_list:
-            """e.g.
-            eliminated = (e, "b")
-            """
-            try:
-                eliminated_state = states[eliminated]
-            except KeyError:
-                continue
-            """e.g.
-            eliminated_state = "rB+"
-            """
-            yield (eliminated_state, [L, C, R], [element1, element2])
