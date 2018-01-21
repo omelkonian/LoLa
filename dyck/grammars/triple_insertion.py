56174adafe93c1d56cab2281c7f2d2f7c74c1b89
Orestis Melkonian <melkon.or@gmail.com>
2018-01-21 22:04:12 +0100

    Removed dyck python dir.

--- dyck/grammars/triple_insertion.py
+++
@@ -1,9 +0,0 @@
-from dyck import Grammar
-from grammar_utils import *
-
-triple_insertion = Grammar([
-    (S, [W], [[x, y]]),
-    all_o(W, e, [[a, b, c]]),
-    all_o(W, [W], [[x, y], [a, b, c]]),
-])
-
